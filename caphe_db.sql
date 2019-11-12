ce2a9f79e64c8597b2d31f8495d9f0213767b126
nah1510 <nah151098@gmail.com>
2019-11-12 21:29:05 +0700

    thêm san pham

--- caphe_db.sql
+++ caphe_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: db
--- Generation Time: Oct 29, 2019 at 06:35 PM
--- Server version: 5.7.22-log
--- PHP Version: 7.2.23
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 12, 2019 lúc 03:21 PM
+-- Phiên bản máy phục vụ: 10.1.38-MariaDB
+-- Phiên bản PHP: 7.2.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,74 +19,117 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `laravel`
+-- Cơ sở dữ liệu: `caphe_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Cấu trúc bảng cho bảng `nhanvien`
 --
 
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `nhanvien` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) CHARACTER SET ucs2 COLLATE ucs2_vietnamese_ci NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `identity_card_number` varchar(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhanvien`
+--
+
+INSERT INTO `nhanvien` (`id`, `name`, `email`, `identity_card_number`, `updated_at`, `created_at`) VALUES
+(2, 'Phạm Bảo Hân', 'test@gmail.com', '123', '2019-11-12 13:04:00', '2019-11-12 13:04:00'),
+(3, 'Phạm Bảo Hân', 'test@gmail.com', '123', '2019-11-12 13:04:03', '2019-11-12 13:04:03');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET ucs2 COLLATE ucs2_vietnamese_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `status` int(1) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `sanpham`
+--
+
+INSERT INTO `sanpham` (`id`, `name`, `price`, `status`, `updated_at`, `created_at`) VALUES
+(3, 'Phạm Bảo Hân', 1, 1, '2019-11-12 10:49:10', '2019-11-08 06:41:08');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `activation_key` varchar(100) DEFAULT NULL
+  `activation_key` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Đang đổ dữ liệu cho bảng `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `activation_key`) VALUES
-(1, 'nah151098@gmail.com', '$2y$12$5wRMVUoAczAqbh0st58wq.0L5vtlu9Tn1Fq.Wn1KnFSBstKwWP22W', ''),
-(2, 'duy511998asd@gmail.com', '$2y$12$5wRMVUoAczAqbh0st58wq.0L5vtlu9Tn1Fq.Wn1KnFSBstKwWP22W', ''),
-(3, 'duyoccho@gmail.com', '$2y$12$sfMjb9aKqCUa.wWFLGFmAe.hn4VTlUYyOoi6yzL5jyFDgahuqSJR.', NULL);
+(1, 'nah151098@gmail.com', '$2y$12$CVBg9NHnzQD71tloQ4i0I.HLFrMJtvOCVL8T4QH0pyfUcFAeTnFxe', '');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `migrations`
+-- Chỉ mục cho bảng `nhanvien`
 --
-ALTER TABLE `migrations`
+ALTER TABLE `nhanvien`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Chỉ mục cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT cho bảng `nhanvien`
 --
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
+ALTER TABLE `nhanvien`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
