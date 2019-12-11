056e0b2c7b0aaa8bb330209af1d2460990213f38
nah1510 <nah151098@gmail.com>
2019-12-11 13:13:28 +0700

    sss

--- QLCaPhe/resources/views/admin/banhang/index.blade.php
+++ QLCaPhe/resources/views/admin/banhang/index.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
+@include('header')
 <body>
+  
     <nav>
         <div class="container">
             <ul class="nav-list">
@@ -12,16 +13,52 @@
             </ul>
         </div>
     </nav>
-
-    @foreach($sanpham as $list)
-    <div>
-    <img src="/upload/{{$list->image}}" class="rounded" alt="Cinque Terre">
-    <h5>{{$list->name}}</h5>
-    <strong>{{$list->price}}</strong>
+    <div class="col-sm-6">
+      @foreach($sanpham as $list)
+      <div class="menu-cafe" value="{{$list->id}}">
+        <img src="upload/{{$list->image}}" class="rounded" alt="Cinque Terre">
+        <h5>{{$list->name}}</h5>
+        <strong>{{$list->price}}</strong>
+      </div>
+      @endforeach
     </div>
-    @endforeach
-    
-
+    <div class="col-sm-6">
+    <table class="table">
+		  <thead class="black">
+			<tr>
+			  <th scope="col">Món</th>
+			  <th scope="col">Số lượng</th>
+			  <th scope="col">Hành Động</th>
+			</tr>
+		  </thead>
+		  <tbody class="table-body">
+			<tr>
+			  <td></td>
+			  <td><input onKeyPress="return isNumberKey(event)" class="number-input" type="text" min="1" max="99" maxlength="2"></td>
+			  <td><button class="btn-delete">Hủy</button></td>
+			</tr>
+		  </tbody>
+		</table>
+    </div>
 </body>
 
 </html>
+<script language='javascript'>
+  
+  $(".menu-cafe").click(function(){
+    var html = '<tr><td>'+$(this).find("h5").text()+'</td>'+
+                '<td><input value="1" onKeyPress="return isNumberKey(event)" class="number-input" type="text" min="1" max="99" maxlength="2"></td>'+
+                '<td><button class="btn-delete">Hủy</button></td></tr>';
+    $(".table-body").append(html);
+    $(".btn-delete").on("click", function() { 
+    $(this).parent().parent().remove();
+  }); 
+  });
+ function isNumberKey(evt)
+ {
+ var charCode = (evt.which) ? evt.which : event.keyCode
+ if (charCode > 31 && (charCode < 48 || charCode > 57))
+ return false;
+ return true;
+ }
+ </script>
