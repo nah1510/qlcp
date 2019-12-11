58f1595235ec197f0f1f9e54e9ed10ecd6aa76df
helloworld518 <duy511998dsa@gmail.com>
2019-12-11 12:26:01 +0700

    add styles sanpham index blade.php

--- QLCaPhe/resources/views/admin/banhang/index.blade.php
+++ QLCaPhe/resources/views/admin/banhang/index.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<head>
+    <link href="{{asset('assets/sass/indexblade.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/globalstyle.css')}}" rel="stylesheet" />
+</head>
+<style>
+    
+</style>
 <body>
     <nav>
         <div class="container">
@@ -13,15 +19,22 @@
         </div>
     </nav>
 
+    <div class="wrapper">
     @foreach($sanpham as $list)
-    <div>
-    <img src="/upload/{{$list->image}}" class="rounded" alt="Cinque Terre">
-    <h5>{{$list->name}}</h5>
-    <strong>{{$list->price}}</strong>
-    </div>
+        <article class="card">
+            <div class= "card-media">
+                <img src="/upload/{{$list->image}}" class="size" alt="Cinque Terre">
+            </div>
+            <div class="card-content">
+                <h2 class="card-content-header">{{$list->name}}</h2>
+                <p class="card-content-price">{{$list->price}}</p>
+            </div>
+        </article>
     @endforeach
+    </div>
+    <script>
     
-
+    </script>
 </body>
 
 </html>
