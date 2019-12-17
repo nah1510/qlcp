c4b75182538a81882e41454e01c329ec8d8e829d
helloworld518 <duy511998dsa@gmail.com>
2019-12-18 04:39:00 +0700

    add styles table result in page banhang and add notification icon in masterhead

--- QLCaPhe/resources/views/admin/banhang/index.blade.php
+++ QLCaPhe/resources/views/admin/banhang/index.blade.php
@@ -4,230 +4,252 @@
 <head>
     <link href="{{asset('assets/sass/indexblade.css')}}" rel="stylesheet" />
     <link href="{{asset('assets/css/globalstyle.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/sass/header.css')}}" rel="stylesheet" />
     <script src="{{asset('js/jquery-3.4.1.js')}}"></script>
 </head>
 <style>
-    
+
 </style>
-<body>
-  <section class="content">
-  <div class="content-leftside">
-    <nav>
-        <div class="wrapNavlist">
-            <ul class="navlist">
-                <div onclick="list_san_pham(0)" class="navlist-item"><li>Tất cả</li></div>
-                @foreach($loaisanpham as $list)
-                <div onclick="list_san_pham({{$list->id}})" class="navlist-item"><li>{{$list->name}}</li></div>
-                @endforeach
-            </ul>
-        </div>
-        <div class="bg"></div>
-    </nav>
 
-  <div class="wrapBackground style-1">
-    <div class="wrapItem" >
+<body class="style-1">
+    @include('masterhead')
+    <section class="content wrapper">
+        <div class="content-leftside">
+            <nav>
+                <div class="wrapNavlist">
+                    <ul class="navlist">
+                        <div onclick="list_san_pham(0)" class="navlist-item">
+                            <li>Tất cả</li>
+                        </div>
+                        @foreach($loaisanpham as $list)
+                        <div onclick="list_san_pham({{$list->id}})" class="navlist-item">
+                            <li>{{$list->name}}</li>
+                        </div>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="bg"></div>
+            </nav>
 
-    </div>
-  </div>
-</div>
-  <div class="content-rightside">
-      <div class="client">
-        <div class="client-leftside"><img src="/upload/icon/bill.svg" alt="Hóa Đơn" class ="Image"/></div>
-        <div class="client-rightside"><img src="/upload/icon/target.svg" alt="khách hàng" class ="Image"/></div>
-      </div>
-<div class="wrapTable left style-1">
-    <table class="table tb">
-          <thead class="black">
-            <tr class="row">
-              <th scope="col" class="head">Món</th>
-              <th scope="col" class="head">Số lượng</th>
-              <th scope="col" class="head">Giá</th>
-              <th scope="col" class="head">Hành Động</th>
-            </tr>
-          </thead>
-          <tbody class="table-body">
-          </tbody>
-        </table>
-    </div>
-    <div class="wrapTable right" style="display:none;">
-    <div class="wrapTag-right">
-        <div class="searchClient">
-        <div class="searchClient-input"><input type="text" id="info-customer"  placeholder="Nhập thông tin khách" class='form-controll'></div>
-        <div class="searchClient-icon"><button onclick="khachhang()"><div src="/upload/icon/searching-data-in-database.svg" class='iconSearch' ></div></button></div>
+            <div class="wrapBackground style-1">
+                <div class="wrapItem">
+
+                </div>
+            </div>
         </div>
-        <div id="show-info-customer">
-            
-        </div>
-        </div>
-    </div>
+        <div class="content-rightside">
+            <div class="client">
+                <div class="client-leftside"><img src="/upload/icon/bill.svg" alt="Hóa Đơn" class="Image" /></div>
+                <div class="client-rightside"><img src="/upload/icon/target.svg" alt="khách hàng" class="Image" /></div>
+            </div>
+            <div class="wrapTable left style-1">
+                <table class="table tb">
+                    <thead class="black">
+                        <tr class="row">
+                            <th scope="col" class="head">Món</th>
+                            <th scope="col" class="head">Số lượng</th>
+                            <th scope="col" class="head">Giá</th>
+                            <th scope="col" class="head">Hành Động</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-body">
+                    </tbody>
+                </table>
+            </div>
+            <div class="wrapTable right" style="display:none;">
+                <div class="wrapTag-right">
+                    <div class="searchClient">
+                        <div class="searchClient-input"><input type="text" id="info-customer"
+                                placeholder="Nhập thông tin khách" class='form-controll'></div>
+                        <div class="searchClient-icon"><button onclick="khachhang()">
+                                <div src="/upload/icon/searching-data-in-database.svg" class='iconSearch'></div>
+                            </button></div>
+                    </div>
+                    <div id="show-info-customer" class="result">
+
+                    </div>
+                </div>
+            </div>
             <div class="wrapSum">
-    <div class="Sum"><div class="Sum-item"><input type="text" id="total_bill" class="form-controll form-controll-sum " name="total_bill" value ="0" readonly><img src='/upload/icon/coins.svg' alt='icon' class="moneyIcon" ></div></div>
-        <div class="Print"><button onclick="save_db()" class="Invoice">Xuất Hóa Đơn</button></div>
-    </div>
-    </div>
-    </div>
+                <div class="Sum">
+                    <div class="Sum-item"><input type="text" id="total_bill" class="form-controll form-controll-sum "
+                            name="total_bill" value="0" readonly><img src='/upload/icon/coins.svg' alt='icon'
+                            class="moneyIcon"></div>
+                </div>
+                <div class="Print"><button onclick="save_db()" class="Invoice">Xuất Hóa Đơn</button></div>
+            </div>
+        </div>
+        </div>
 
     </section>
     <input type="hidden" id="customer_id">
 </body>
+
 </html>
 <script language='javascript'>
 list_san_pham(0);
 
-    function isNumberKey(evt) {
-        var charCode = (evt.which) ? evt.which : event.keyCode
-        if (charCode > 31 && (charCode < 48 || charCode > 57))
-            return false;
-        return true;
-        total_bill();
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+    total_bill();
+}
+
+function change0(id) {
+    var row = $(".table-body").find("#" + id);
+    if (Number(row.find(".number-input").val()) == 0) {
+        row.remove();
+    };
+    total_bill();
+}
+
+function total_bill() {
+    var total_bill = 0;
+    $('.tr').each(function() {
+        total_bill += Number($(this).find('.number-input').val()) * Number($(this).find('.price').val());
+
+    });
+    $("#total_bill").val(total_bill);
+}
+
+function save_db() {
+    if ($("#total_bill").val() == 0) {
+        alert("Not create bill");
+        return;
     }
-
-    function change0(id) {
-        var row = $(".table-body").find("#" + id);
-        if (Number(row.find(".number-input").val()) == 0) {
-            row.remove();
-        };
-        total_bill();
-    }
-
-    function total_bill() {
-        var total_bill = 0;
-        $('.tr').each(function() {
-            total_bill += Number($(this).find('.number-input').val()) * Number($(this).find('.price').val());
-
-        });
-        $("#total_bill").val(total_bill);
-    }
-
-    function save_db() {
-        if ($("#total_bill").val() == 0) {
-            alert("Not create bill");
-            return;
+    var HoaDon = [];
+    $('.tr').each(function() {
+        var total = Number($(this).find('.number-input').val()) * Number($(this).find('.price').val());
+        var CT_HoaDon = [$(this).attr('id'), $(this).find('.price').val(), $(this).find('.number-input').val(),
+            total
+        ];
+        HoaDon.push(CT_HoaDon);
+    });
+    alert($("#customer_id").val());
+    $.ajax({
+        type: 'POST',
+        url: 'ajax_save_bill',
+        data: {
+            _token: "{{ csrf_token() }}",
+            total_bill: $("#total_bill").val(),
+            bill: HoaDon,
+            customer_id: $("#customer_id").val(),
+        },
+        success: function(msg) {
+            alert("Đã thêm thành công " + msg);
+            $(".table-body >tr").remove();
+            total_bill();
+            list_san_pham(0);
         }
-        var HoaDon = [];
-        $('.tr').each(function() {
-            var total = Number($(this).find('.number-input').val()) * Number($(this).find('.price').val());
-            var CT_HoaDon = [$(this).attr('id'), $(this).find('.price').val(), $(this).find('.number-input').val(), total];
-            HoaDon.push(CT_HoaDon);
-        });
-        alert($("#customer_id").val());
-        $.ajax({
-            type: 'POST',
-            url: 'ajax_save_bill',
-            data: {
-                _token: "{{ csrf_token() }}",
-                total_bill: $("#total_bill").val(),
-                bill: HoaDon,
-                customer_id: $("#customer_id").val(),
-            },
-            success: function(msg) {
-                alert("Đã thêm thành công " + msg);
-                $(".table-body >tr").remove();
+    });
+}
+
+function list_san_pham(id) {
+    $(".wrapItem >article").remove();
+    $.ajax({
+        type: 'POST',
+        url: 'ajax_list_san_pham',
+        data: {
+            _token: "{{ csrf_token() }}",
+            id: id,
+        },
+        success: function(data) {
+            data = JSON.parse(data);
+            $.each(data, function(key, value) {
+                var html = '<article class="card menu-cafe">' +
+                    '<input class="id-product" type="hidden" value="' + value['id'] + '">' +
+                    '<input class="price-product" type="hidden" value="' + value['price'] + '">' +
+                    '<div class= "card-media">' +
+                    '<img src="/upload/' + value['image'] + '" class="size" >' +
+                    '<p class="card-media-price">' + value['price'].toLocaleString().replace(
+                        /\d(?=(\d{3})+\.)/g, '$&,') + ' VND</p>' +
+                    '</div >' +
+                    '<div class="card-content">' +
+                    '<h2 class="card-content-header">' + value['name'] + '</h2>' +
+                    '</div>' +
+                    '</article>';
+                $(".wrapItem").append(html);
+            });
+            $(".menu-cafe").on("click", function() {
+                var id = $(this).find(".id-product").val();
+                var row = $(".table").find("#" + id);
+                if (row.length) {
+                    row.find(".number-input").val(Number(row.find(".number-input").val()) + 1);
+                    total_bill();
+                    return;
+                }
+                var html = '<tr class="tr rowItem" id="' + id + '"><td>' + $(this).find("h2")
+                    .text() + '</td>' +
+                    '<td ><div class="dataInput"><div><input value="1" onchange="change0(' + id +
+                    ')" onKeyPress="return isNumberKey(event)" class="number-input numberInput form-controll" type="text" min="1" max="99" maxlength="2"></div><div><button class="plus btn btn-Plus">+</button><button class="sub btn btn-Sub">-</button></div></div></td>' +
+                    '<td class="center">' + $(this).find(".price-product").val() +
+                    '<input type="hidden" class="price" value="' + $(this).find(".price-product")
+                    .val() + '"></td>' +
+                    '<td><button class="btnD btn-delete">Hủy</button></td></tr>';
+                $(".table-body").append(html);
+                $(".btn-delete").on("click", function() {
+                    $(this).parent().parent().remove();
+                    total_bill();
+                });
+                $(".plus,.sub").off("click");
+                $(".plus").on("click", function() {
+                    var number = $(this).parent().parent().find('.number-input');
+                    if (Number(number.val()) == 99)
+                        return;
+                    number.val(Number(number.val()) + 1);
+                    total_bill();
+                });
+                $(".sub").on("click", function() {
+                    var number = $(this).parent().parent().find('.number-input');
+                    if (Number(number.val()) == 1) {
+                        // $(this).parent().parent().parent().remove();
+                        return;
+                    }
+                    number.val(Number(number.val()) - 1);
+                    total_bill();
+                });
                 total_bill();
-                list_san_pham(0);
-            }
-        });
-    }
-    function list_san_pham(id){
-        $(".wrapItem >article").remove();
-        $.ajax({
-            type: 'POST',
-            url: 'ajax_list_san_pham',
-            data: {
-                _token: "{{ csrf_token() }}",
-                id:id,
-            },
-            success: function(data) {
-                data = JSON.parse(data);
-                $.each(data, function(key, value) {
-                        var html='<article class="card menu-cafe">'+
-                        '<input class="id-product" type="hidden" value="'+value['id']+'">'+
-                        '<input class="price-product" type="hidden" value="'+value['price']+'">'+
-            '<div class= "card-media">'+
-            '<img src="/upload/'+value['image']+'" class="size" >'+
-            '<p class="card-media-price">'+value['price'].toLocaleString().replace(/\d(?=(\d{3})+\.)/g, '$&,')+' VND</p>'
-                +'</div >'+
-            '<div class="card-content">'+
-            '<h2 class="card-content-header">'+value['name']+'</h2>'+
-                '</div>'+
-            '</article>';
-            $(".wrapItem").append(html); 
-                    });
-    $(".menu-cafe").on("click", function(){
-        var id = $(this).find(".id-product").val();
-        var row = $(".table").find("#" + id);
-        if (row.length) {
-            row.find(".number-input").val(Number(row.find(".number-input").val()) + 1);
-            total_bill();
-            return;
+            });
         }
-        var html = '<tr class="tr rowItem" id="' + id + '"><td>' + $(this).find("h2").text() + '</td>' +
-            '<td ><div class="dataInput"><div><input value="1" onchange="change0(' + id + ')" onKeyPress="return isNumberKey(event)" class="number-input numberInput form-controll" type="text" min="1" max="99" maxlength="2"></div><div><button class="plus btn btn-Plus">+</button><button class="sub btn btn-Sub">-</button></div></div></td>' +
-            '<td class="center">' + $(this).find(".price-product").val() + '<input type="hidden" class="price" value="' + $(this).find(".price-product").val() + '"></td>' +
-            '<td><button class="btnD btn-delete">Hủy</button></td></tr>';
-        $(".table-body").append(html);
-        $(".btn-delete").on("click", function() {
-            $(this).parent().parent().remove();
-            total_bill();
-        });
-        $( ".plus,.sub" ).off( "click");
-        $(".plus").on("click", function() {
-            var number = $(this).parent().parent().find('.number-input');
-            if (Number(number.val()) == 99)
-                return;
-            number.val(Number(number.val()) + 1);
-            total_bill();
-        });
-        $(".sub").on("click", function() {
-            var number = $(this).parent().parent().find('.number-input');
-            if (Number(number.val()) == 1) {
-                // $(this).parent().parent().parent().remove();
-                return;
-            }
-            number.val(Number(number.val()) - 1);
-            total_bill();
-        });
-        total_bill();
     });
+}
+$(".client-leftside").click(function() {
+    $(".left").show();
+    $(".right").hide();
+});
+$(".client-rightside").click(function() {
+    $(".right").show();
+    $(".left").hide();
+});
+
+function khachhang() {
+    if ($('#info-customer').val() == "")
+        return;
+    $.ajax({
+        type: 'POST',
+        url: 'ajax_find_customer',
+        data: {
+            _token: "{{ csrf_token() }}",
+            phone: $("#info-customer").val(),
+        },
+        success: function(data) {
+            $("#show-info-customer > div").remove();
+            //$("#show-info-customer >div").remove();  nếu là div
+            if (data == "false") {
+                var html =
+                    '<div class="notFound"><div class="notFound-icon"><img src="/upload/icon/not-found.svg" alt="not found"></div><div class="notFound-text">Không tìm thấy kết quả phù hợp</div></div>';
+                $("#customer_id").val("");
+                $("#show-info-customer").append(html);
+            } else {
+                data = JSON.parse(data);
+                var html = '<div class="inform"><h6>' + data["name"] + '</h6><h6>' + data["name"] +
+                    '</h6><div>';
+                $("#show-info-customer").append(html);
+                $("#customer_id").val(data["id"]);
             }
-        });
-    }
-    $(".client-leftside").click(function(){
-        $(".left").show();
-        $(".right").hide();
+        }
     });
-    $(".client-rightside").click(function(){
-        $(".right").show();
-        $(".left").hide();
-    });
-    function khachhang(){
-        if($('#info-customer').val()=="")
-            return;
-        $.ajax({
-            type: 'POST',
-            url: 'ajax_find_customer',
-            data: {
-                _token: "{{ csrf_token() }}",
-                phone: $("#info-customer").val(),
-            },
-            success: function(data) {
-                $("#show-info-customer >h6").remove(); 
-                //$("#show-info-customer >div").remove();  nếu là div
-                if(data=="false")
-                    {   
-                        var html = '<h6>Đéo tìm ra</h6>';
-                        $("#customer_id").val(""); 
-                        $("#show-info-customer").append(html); 
-                    }
-                    
-                else
-                    {
-                        data = JSON.parse(data);
-                            var html = '<h6>'+data["name"]+'</h6>';
-                            $("#show-info-customer").append(html); 
-                            $("#customer_id").val(data["id"]); 
-                    }
-            }
-        });
-    }
+}
 </script>
--- QLCaPhe/resources/views/masterhead.blade.php
+++ QLCaPhe/resources/views/masterhead.blade.php
@@ -1,34 +1,37 @@
 <div class="header">
-<div class="header-left"><a href="/"><img src='/upload/logo/coffeeLogo.svg' alt="logo" class="applogo"></a></div>
-<div class="header-right"><div class="User">
-  <span class="User-logo"><img src="/upload/icon/man-user.svg" alt="user" /></span>
-  <span class="User-name">Chào {{Auth::user()->name}} !</span> 
-  <!-- <a class="btn btn-primary" href="/{{ url('/logout') }}">Đăng xuất</a> -->
-  <div class="menu">
-      <div class="menu-virtual"></div>
-  <a>
-  <div class="menu-item firstchild">
-      
-    <div class="menu-item-icon">
-        <img src="/upload/icon/target.svg" alt="thông tin">
-    </div> 
-    <div class="menu-item-content">
-        Thông tin
+    <div class="header-left"><a href="/"><img src='/upload/logo/coffeeLogo.svg' alt="logo" class="applogo"></a></div>
+    <div class="header-right">
+        <div class="Notification">
+            <div class="Notification-icon"><img src="/upload/icon/notification.svg"></div>
+        </div>
+        <div class="User">
+            <span class="User-logo"><img src="/upload/icon/man-user.svg" alt="user" /></span>
+            <span class="User-name">Chào {{Auth::user()->name}} !</span>
+            <!-- <a class="btn btn-primary" href="/{{ url('/logout') }}">Đăng xuất</a> -->
+            <div class="menu">
+                <div class="menu-virtual"></div>
+                <a>
+                    <div class="menu-item firstchild">
+                        <div class="menu-item-icon">
+                            <img src="/upload/icon/target.svg" alt="thông tin">
+                        </div>
+                        <div class="menu-item-content">
+                            Thông tin
+                        </div>
+                    </div>
+                </a>
+                <a href="/logout" style="color: inherit; text-decoration: none;">
+                    <div class="menu-item">
+
+                        <div class="menu-item-icon">
+                            <img src="/upload/icon/logout.svg" alt="Đăng xuất">
+                        </div>
+                        <div class="menu-item-content">
+                            Đăng xuất
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
-        
-</div>
-</a>
-<a href="/logout" style="color: inherit; text-decoration: none;">
-    <div class="menu-item">
-        
-    <div class="menu-item-icon">
-        <img src="/upload/icon/logout.svg" alt="Đăng xuất">
-    </div> 
-    <div class="menu-item-content">
-        Đăng xuất
-    </div> 
-    </div>
-</a>
-  </div>
-  </div></div>
 </div>
