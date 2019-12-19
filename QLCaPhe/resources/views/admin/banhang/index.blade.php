604c8fc857983a2e9e487688b46a875569e36110
helloworld518 <duy511998dsa@gmail.com>
2019-12-20 03:44:34 +0700

    fix styles banhang page

--- QLCaPhe/resources/views/admin/banhang/index.blade.php
+++ QLCaPhe/resources/views/admin/banhang/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="vi">
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{asset('assets/sass/indexblade.css')}}" rel="stylesheet" />
     <link href="{{asset('assets/css/globalstyle.css')}}" rel="stylesheet" />
     <link href="{{asset('assets/sass/header.css')}}" rel="stylesheet" />
